f2005a80c81f0a8d311c0f6862774c96422b9b13
Paul Grey <greyp9@yahoo.com>
2023-12-19 18:16:34 -0500

    NIFI-11259 - missed a spot

--- nifi-nar-bundles/nifi-kafka-bundle/nifi-kafka-2-6-integration/src/test/java/org/apache/nifi/processors/kafka/publish/it/PublishKafka_2_6_BaseIT.java
+++ nifi-nar-bundles/nifi-kafka-bundle/nifi-kafka-2-6-integration/src/test/java/org/apache/nifi/processors/kafka/publish/it/PublishKafka_2_6_BaseIT.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.nifi.processors.kafka.publish.it;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -17,6 +33,7 @@ import java.time.Duration;
 import java.util.Properties;
 
 public class PublishKafka_2_6_BaseIT {
+    // https://docs.confluent.io/platform/current/installation/versions-interoperability.html#cp-and-apache-kafka-compatibility
     public static final String IMAGE_NAME = "confluentinc/cp-kafka:6.1.14";  // Kafka 2.7
 
     public static final Duration DURATION_POLL = Duration.ofMillis(1000L);
